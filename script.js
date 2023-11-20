9b720dbe1758b040b34bc39c0ff79338acd841f0
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-20 17:25:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+  const firstName = document.getElementsByName('fname')[0].value;
+  const lastName = document.getElementsByName('lname')[0].value;
 
+  alert(`${firstName} ${lastName}`);
 }
